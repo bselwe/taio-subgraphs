f0b3f3fa53fd980769b7b40f48dc4aaf6fccf9f7
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2018-11-08 20:32:50 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TAiO.Subgraphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
